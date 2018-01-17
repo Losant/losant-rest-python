cb6bed3497fab64c543c6f81291c5190a26c6daf
Michael Kuehl <michael@losant.com>
2018-01-17 15:19:57 -0500

    api release 1.9.3 (python client 1.4.2)

--- losantrest/client.py
+++ losantrest/client.py
@@ -87,7 +87,7 @@ class Client(object):
 
     User API for accessing Losant data
 
-    Built For Version 1.9.2
+    Built For Version 1.9.3
     """
 
     def __init__(self, auth_token=None, url="https://api.losant.com"):
@@ -150,7 +150,7 @@ class Client(object):
             params = {}
 
         headers["Accept"] = "application/json"
-        headers["Accept-Version"] = "^1.9.2"
+        headers["Accept-Version"] = "^1.9.3"
         if self.auth_token:
             headers["Authorization"] = "Bearer {0}".format(self.auth_token)
 
