688cc086297de64fb64b47b72544ad4bcf3c0b8a
eeerin <erin@losant.com>
2020-03-18 15:52:19 -0400

    regenerating client for v1.10.0

--- losantrest/client.py
+++ losantrest/client.py
@@ -98,7 +98,7 @@ class Client(object):
 
     User API for accessing Losant data
 
-    Built For Version 1.16.4
+    Built For Version 1.17.0
     """
 
     def __init__(self, auth_token=None, url="https://api.losant.com"):
@@ -172,7 +172,7 @@ class Client(object):
             params = {}
 
         headers["Accept"] = "application/json"
-        headers["Accept-Version"] = "^1.16.4"
+        headers["Accept-Version"] = "^1.17.0"
         if self.auth_token:
             headers["Authorization"] = "Bearer {0}".format(self.auth_token)
 
