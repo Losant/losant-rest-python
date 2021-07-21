737f18a2530a508f8eabd0005f95767d8a781148
eeerin <erin@losant.com>
2021-07-21 14:06:57 -0400

    add charset-normalizer so tests will pass

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     include_package_data=True,
     keywords=['REST', 'Losant', 'IoT'],
     test_suite='tests',
-    install_requires=['requests>=2.13'],
+    install_requires=['requests>=2.13', 'charset-normalizer'],
     tests_require=['requests-mock>=1.9.0'],
 )
